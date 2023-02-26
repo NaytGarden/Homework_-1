dd3890bef5ff21cf14fbb27179745b8b647cac63
Nathaniel <na26435@mail.ru>
2023-02-26 13:05:20 +0300

    Задача 2

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+/* ЗАДАЧА 2
+    Напишите программу, которая на вход принимает два числа и выдает,
+какое число больше, а какое меньше*/
+
+Console.WriteLine("Введите число");
+int a = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите число");
+int b = int.Parse(Console.ReadLine());
+int max = a;
+
+if(a == b){Console.WriteLine(max);}
+if(a < b){max = b; Console.WriteLine(max);} else{Console.WriteLine(max);}
